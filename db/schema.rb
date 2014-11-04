d6e3d01b1ecd699d76df8ad3b089efc80814c941
kkane106 <kkane106@gmail.com>
2014-11-04 12:05:39 -0500

    Correct spelling error on vendor asset file name "underscore.js"

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20141104170313) do
+
+  create_table "locations", force: true do |t|
+    t.string   "address"
+    t.float    "latitude"
+    t.float    "longitude"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
