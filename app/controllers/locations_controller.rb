92167cbc00f1f1425f64c8c1043b309e681ba7e7
kkane106 <kkane106@gmail.com>
2014-11-04 11:49:57 -0500

    add show action add index action

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,5 +1,13 @@
 class LocationsController < ApplicationController
 
+	def index
+		@locations = Location.all
+	end
+
+	def show
+		@location = Location.find(params[:id])
+	end
+
 	def new
 		@location = Location.new
 	end
